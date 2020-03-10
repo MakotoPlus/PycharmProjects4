85b1886cdfa95112e7ee0f56be6d70428890d4c4
makoto <fukupeemail@gmail.com>
2020-03-11 00:19:16 +0900

    loogeer

--- Recruitment/applicantctl/util/logger.py
+++ Recruitment/applicantctl/util/logger.py
@@ -10,24 +10,9 @@ def logger(func_name):
             log.info(func_name + "() 開始")
             #今まで実行していたSQLをクリア
             reset_queries()
-            try:
-                #実際のメソッド呼出し
-                result = func(*args, **kwargs)
-            except Exception  as e:
-                #とりあえずSQLは出力する
-                sql_list = connections['default'].queries
-                for sql in sql_list:
-                    #今まで実行していたSQLをログへ出力
-                    log.debug(sql)
-                #例外情報ログ出力
-                log.exception(e)
-                log.info(func_name + "() 終了")
-                #例外が発生したらエラー画面へ遷移
-                content = {
-                    'error' : e
-                }
-                #エラー画面へ遷移
-                return render(args[0], 'applicantctl/err.html', content )
+
+            #実際のメソッド呼出し
+            result = func(*args, **kwargs)
             sql_list = connections['default'].queries
             for sql in sql_list:
                 #今まで実行していたSQLをログへ出力
@@ -35,4 +20,29 @@ def logger(func_name):
             log.info(func_name + "() 終了")
             return result
         return inner
+#            try:
+#                #実際のメソッド呼出し
+#                result = func(*args, **kwargs)
+#            except Exception  as e:
+#                #とりあえずSQLは出力する
+#                sql_list = connections['default'].queries
+#                for sql in sql_list:
+#                    #今まで実行していたSQLをログへ出力
+#                    log.debug(sql)
+#                #例外情報ログ出力
+#                log.exception(e)
+#                log.info(func_name + "() 終了")
+#                #例外が発生したらエラー画面へ遷移
+#                content = {
+#                    'error' : e
+#                }
+#                #エラー画面へ遷移
+#                return render(args[0], 'applicantctl/err.html', content )
+#            sql_list = connections['default'].queries
+#            for sql in sql_list:
+#                #今まで実行していたSQLをログへ出力
+#                log.debug(sql)
+#            log.info(func_name + "() 終了")
+#            return result
+#        return inner
     return __decorator
